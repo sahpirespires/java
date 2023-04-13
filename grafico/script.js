b99896bbbaa4a986ee45700cef11b27192858000
sahpirespires <124930619+sahpirespires@users.noreply.github.com>
2023-04-13 14:42:08 -0300

    Add files via upload

---
+++ grafico/script.js
@@ -0,0 +1,87 @@
+
+var button = document.getElementById("inp");
+//chamando o botão//
+
+button.addEventListener ("click", function(){
+    //está fazendo um evento para o botão, que quando clicar, irá fazer algo//
+    var inp1 = document.getElementById ("inp1");
+    var valor1 = inp1.value;
+//está guardando as informações na variável valor 1//
+    var div1 = document.getElementById("div1");
+    div1.style.height = valor1 + "px";
+    //está pegando as informações da div1 inteira e modificando a altura de acordo com o valor guardado, que será o px modificado no gráfico//
+
+    var inp2 = document.getElementById ("inp2");
+    var valor2 = inp2.value;
+
+    var div2 = document.getElementById("div2");
+    div2.style.height = valor2 + "px";
+
+    
+    var inp3 = document.getElementById ("inp3");
+    var valor3 = inp3.value;
+
+    var div3 = document.getElementById("div3");
+    div3.style.height = valor3 + "px";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+    
+    var inp4 = document.getElementById ("inp4");
+    var valor4 = inp4.value;
+
+    var div4 = document.getElementById("div4");
+    div4.style.height = valor4 + "px";
+
+    
+    var inp5 = document.getElementById ("inp5");
+    var valor5 = inp5.value;
+
+    var div5 = document.getElementById("div5");
+    div5.style.height = valor5 + "px";
+
+
+
+
+
+
+
+
+
+
+})
